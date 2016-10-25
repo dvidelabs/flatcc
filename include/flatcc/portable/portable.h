a75e5aad91842193ac78fc24f0e5eaea909dca00
Mikkel Fahnøe Jørgensen <mikkel@dvide.com>
2016-10-25 12:03:55 +0200

    Don't assume C11 compilers have C11 headers - always use portable feature headers

--- include/flatcc/portable/portable.h
+++ include/flatcc/portable/portable.h
@@ -3,23 +3,14 @@
 
 #include "pversion.h"
 #include "pwarnings.h"
-#include <assert.h>
 
-#if !defined(PORTABLE_STANDALONE) && \
-    (defined(__STDC__) && __STDC__ && defined(__STDC_VERSION__) && __STDC_VERSION__ >= 201112L) 
-/* C11 or newer */
-#include <inttypes.h>
-#include <stdalign.h>
-#ifndef static_assert
-#define static_assert(pred, msg) _Static_assert(pred, msg)
-#else
+/* Featutures that ought to be supported by C11, but some aren't. */
 #include "pinttypes.h"
 #include "pstdalign.h"
 #include "pinline.h"
 #include "pstatic_assert.h"
-#endif
-#endif
 
+/* These are not supported by C11 and are general platform abstractions. */
 #include "pendian.h"
 #include "punaligned.h"
 
