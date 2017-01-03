b24a4380e3ad3fc33b71dc9b5c887a1088e9526c
Mikkel Fahnøe Jørgensen <mikkel@dvide.com>
2017-01-03 22:36:02 +0100

    Add scope counter to avoid __LINE__ conflicts in static_assert

--- include/flatcc/portable/pstatic_assert.h
+++ include/flatcc/portable/pstatic_assert.h
@@ -1,3 +1,8 @@
+/* Outside include guard. */
+#ifndef __COUNTER__
+#include "pstatic_assert_scope.h"
+#endif
+
 #ifndef PSTATIC_ASSERT_H
 #define PSTATIC_ASSERT_H
 
@@ -15,12 +20,12 @@
 
 #else
 
-#define __PSTATIC_ASSERT_CONCAT_(a, b) a##b
+#define __PSTATIC_ASSERT_CONCAT_(a, b) static_assert_scope_##a##_line_##b
 #define __PSTATIC_ASSERT_CONCAT(a, b) __PSTATIC_ASSERT_CONCAT_(a, b)
 #ifdef __COUNTER__
-#define static_assert(e, msg) enum { __PSTATIC_ASSERT_CONCAT(static_assertion_, __COUNTER__) = 1/(!!(e)) }
+#define static_assert(e, msg) enum { __PSTATIC_ASSERT_CONCAT(__COUNTER__, __LINE__) = 1/(!!(e)) }
 #else
-#define static_assert(e, msg) enum { __PSTATIC_ASSERT_CONCAT(static_assert_line_, __LINE__) = 1/(!!(e)) }
+#define static_assert(e, msg) enum { __PSTATIC_ASSERT_CONCAT(__PSTATIC_ASSERT_COUNTER, __LINE__) = 1/(!!(e)) }
 #endif
 
 #endif /* __has_feature */
