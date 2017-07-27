8fbce195fd6883a4fc2533e749169a264ca054e6
Mikkel Fahnøe Jørgensen <mikkel@dvide.com>
2017-07-27 17:26:54 +0200

    Clarify comment on overriding runtime allocation methods

--- include/flatcc/flatcc_alloc.h
+++ include/flatcc/flatcc_alloc.h
@@ -2,12 +2,41 @@
 #define FLATCC_ALLOC_H
 
 /*
- * These allocation abstractions are only for runtime libraries.
- * The flatcc compiler uses Posix allocation routines.
+ * These allocation abstractions are __only__ for runtime libraries.
+ *
+ * The flatcc compiler uses Posix allocation routines regardless
+ * of how this file is configured.
  *
  * This header makes it possible to use systems where malloc is not
  * valid to use. In this case the portable library will not help
  * because it implements Posix / C11 abstractions.
+ *
+ * Systems like FreeRTOS do not work with Posix memory calls and here it
+ * can be helpful to override runtime allocation primitives.
+ *
+ * In general, it is better to customize the allocator and emitter via
+ * flatcc_builder_custom_init and to avoid using the default emitter
+ * specific high level calls the copy out a buffer that must later be
+ * deallocated. This provides full control of allocation withou the need
+ * for this file.
+ *
+ *
+ * IMPORTANT
+ *
+ * If you override malloc, free, etc., make sure your applications
+ * use the same allocation methods. For example, samples/monster.c
+ * and several test cases are no longer guaranteed to work out of the
+ * box.
+ *
+ * The changes must only affect target runtime compilation including the
+ * the runtime library libflatccrt.
+ *
+ * The host system flatcc compiler and the compiler library libflatcc
+ * should NOT be compiled with non-Posix allocation since the compiler
+ * has a dependency on the runtime library and the wrong free operation
+ * might be callled. The safest way to avoid this problem this is to
+ * compile flatcc with the CMake script and the runtime files with a
+ * dedicated build system for the target system.
  */
 
 #include <stdlib.h>
