3b39ef7f3913d62d54a962eea1e8989f53c9a21d
Mikkel Fahnøe Jørgensen <mikkel@dvide.com>
2018-02-19 17:03:31 +0100

    Bump version to 0.5.2

--- include/flatcc/flatcc_version.h
+++ include/flatcc/flatcc_version.h
@@ -2,12 +2,12 @@
 extern "C" {
 #endif
 
-#define FLATCC_VERSION_TEXT "0.5.1-pre"
+#define FLATCC_VERSION_TEXT "0.5.1"
 #define FLATCC_VERSION_MAJOR 0
 #define FLATCC_VERSION_MINOR 5
 #define FLATCC_VERSION_PATCH 1
 /* 1 or 0 */
-#define FLATCC_VERSION_RELEASED 0
+#define FLATCC_VERSION_RELEASED 1
 
 #ifdef __cplusplus
 }
