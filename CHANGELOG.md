137eecf854757f4a1f3ecc595e0986ffea29dccf
Mikkel Fahnøe Jørgensen <mikkel@dvide.com>
2018-08-28 13:08:46 +0200

    Bump version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,7 @@
 # Change Log
 
+## [0.5.3-pre]
+
 ## [0.5.2]
 
 - Handle union vectors in binary schema generation (.bfbs).
--- include/flatcc/flatcc_version.h
+++ include/flatcc/flatcc_version.h
@@ -2,12 +2,12 @@
 extern "C" {
 #endif
 
-#define FLATCC_VERSION_TEXT "0.5.2"
+#define FLATCC_VERSION_TEXT "0.5.3-pre"
 #define FLATCC_VERSION_MAJOR 0
 #define FLATCC_VERSION_MINOR 5
-#define FLATCC_VERSION_PATCH 2
+#define FLATCC_VERSION_PATCH 3
 /* 1 or 0 */
-#define FLATCC_VERSION_RELEASED 1
+#define FLATCC_VERSION_RELEASED 0
 
 #ifdef __cplusplus
 }
