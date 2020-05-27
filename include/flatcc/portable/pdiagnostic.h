0702bbf9d38f799c695b2b693d02923d9e47de22
James Cook <jcook@lgsinnovations.com>
2020-05-27 15:27:50 -0700

    Some additional warning cleanup (#145)
    Clean some undefined preprocessor symbol warnings

--- include/flatcc/portable/pdiagnostic.h
+++ include/flatcc/portable/pdiagnostic.h
@@ -18,14 +18,27 @@
 #pragma warning(disable: 4668) /* preprocessor name not defined */
 #endif
 
-#if defined(_MSC_VER) && !defined(PDIAGNOSTIC_AWARE_MSVC)
+#if !defined(PDIAGNOSTIC_AWARE_MSVC) && defined(_MSC_VER)
 #define PDIAGNOSTIC_AWARE_MSVC 1
-#elif defined(__clang__) && !defined(PDIAGNOSTIC_AWARE_CLANG)
+#elif !defined(PDIAGNOSTIC_AWARE_MSVC)
+#define PDIAGNOSTIC_AWARE_MSVC 0
+#endif
+
+#if !defined(PDIAGNOSTIC_AWARE_CLANG) && defined(__clang__)
 #define PDIAGNOSTIC_AWARE_CLANG 1
+#elif !defined(PDIAGNOSTIC_AWARE_CLANG)
+#define PDIAGNOSTIC_AWARE_CLANG 0
+#endif
+
+#if !defined(PDIAGNOSTIC_AWARE_GCC) && defined(__GNUC__) && !defined(__clang__)
 /* Can disable some warnings even if push is not available (gcc-4.2 vs gcc-4.7) */
-#elif ((__GNUC__ > 4) || (__GNUC__ == 4 && __GNUC_MINOR__ >= 2)) && \
-        !defined(PDIAGNOSTIC_AWARE_GCC)
+#if (__GNUC__ > 4) || (__GNUC__ == 4 && __GNUC_MINOR__ >= 2)
 #define PDIAGNOSTIC_AWARE_GCC 1
+#endif
+#endif
+
+#if !defined(PDIAGNOSTIC_AWARE_GCC)
+#define PDIAGNOSTIC_AWARE_GCC 0
 #endif
 
 #if defined(PDIAGNOSTIC_IGNORE_UNUSED_FUNCTION) || defined(PDIAGNOSTIC_IGNORE_UNUSED)
