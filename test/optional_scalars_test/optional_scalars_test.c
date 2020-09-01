ea448e5df73b473bef4b5640f14b99283d4a6667
Mikkel Fahnøe Jørgensen <mikkel@dvide.com>
2020-09-01 19:17:03 +0200

    Add more test cases for optional scalar test

--- test/optional_scalars_test/optional_scalars_test.c
+++ test/optional_scalars_test/optional_scalars_test.c
@@ -23,6 +23,26 @@ int create_scalar_stuff(flatcc_builder_t *B)
     ns(ScalarStuff_maybe_i8_add(B, 11));
     ns(ScalarStuff_default_i8_add(B, 12));
 
+    ns(ScalarStuff_just_i16_add(B, 42));
+    ns(ScalarStuff_maybe_i16_add(B, 42));
+    ns(ScalarStuff_default_i16_add(B, 42));
+
+    ns(ScalarStuff_just_u32_add(B, 0));
+    ns(ScalarStuff_maybe_u32_add(B, 0));
+    ns(ScalarStuff_default_u32_add(B, 0));
+
+    ns(ScalarStuff_just_f32_add(B, 42));
+    ns(ScalarStuff_maybe_f32_add(B, 42));
+    ns(ScalarStuff_default_f32_add(B, 42));
+
+    ns(ScalarStuff_just_bool_add(B, 1));
+    ns(ScalarStuff_maybe_bool_add(B, 1));
+    ns(ScalarStuff_default_bool_add(B, 1));
+
+    ns(ScalarStuff_just_enum_add)(B, ns(OptionalByte_One));
+    ns(ScalarStuff_maybe_enum_add)(B, ns(OptionalByte_One));
+    ns(ScalarStuff_default_enum_add)(B, ns(OptionalByte_One));
+
     ns(ScalarStuff_end_as_root(B));
 
     return 0;
@@ -32,12 +52,16 @@ int access_scalar_stuff(const void *buffer)
 {
     ns(ScalarStuff_table_t) t = ns(ScalarStuff_as_root(buffer));
     flatbuffers_int8_option_t maybe_i8;
+    flatbuffers_int16_option_t maybe_i16;
+    flatbuffers_uint32_option_t maybe_u32;
+    flatbuffers_uint8_option_t maybe_u8;
+    flatbuffers_float_option_t maybe_f32;
+    flatbuffers_bool_option_t maybe_bool;
     ns(OptionalByte_option_t) maybe_enum;
 
     test_assert(10 == ns(ScalarStuff_just_i8_get(t)));
     test_assert(11 == ns(ScalarStuff_maybe_i8_get(t)));
     test_assert(12 == ns(ScalarStuff_default_i8_get(t)));
-
     maybe_i8 = ns(ScalarStuff_maybe_i8_option(t));
     test_assert(!maybe_i8.is_null);
     test_assert(11 == maybe_i8.value);
@@ -45,8 +69,64 @@ int access_scalar_stuff(const void *buffer)
     test_assert(ns(ScalarStuff_maybe_i8_is_present(t)));
     test_assert(ns(ScalarStuff_default_i8_is_present(t)));
 
+    test_assert(0 == ns(ScalarStuff_just_u8_get(t)));
+    test_assert(0 == ns(ScalarStuff_maybe_u8_get(t)));
+    test_assert(42 == ns(ScalarStuff_default_u8_get(t)));
+    maybe_u8 = ns(ScalarStuff_maybe_u8_option(t));
+    test_assert(maybe_u8.is_null);
+    test_assert(0 == maybe_u8.value);
+    test_assert(!ns(ScalarStuff_just_u8_is_present(t)));
+    test_assert(!ns(ScalarStuff_maybe_u8_is_present(t)));
+    test_assert(!ns(ScalarStuff_default_u8_is_present(t)));
+
+    test_assert(42 == ns(ScalarStuff_just_i16_get(t)));
+    test_assert(42 == ns(ScalarStuff_maybe_i16_get(t)));
+    test_assert(42 == ns(ScalarStuff_default_i16_get(t)));
+    maybe_i16 = ns(ScalarStuff_maybe_i16_option(t));
+    test_assert(!maybe_i16.is_null);
+    test_assert(42 == maybe_i16.value);
+    test_assert(ns(ScalarStuff_just_i16_is_present(t)));
+    test_assert(ns(ScalarStuff_maybe_i16_is_present(t)));
+    test_assert(!ns(ScalarStuff_default_i16_is_present(t)));
+
+    test_assert(0 == ns(ScalarStuff_just_u32_get(t)));
+    test_assert(0 == ns(ScalarStuff_maybe_u32_get(t)));
+    test_assert(0 == ns(ScalarStuff_default_u32_get(t)));
+    maybe_u32 = ns(ScalarStuff_maybe_u32_option(t));
+    test_assert(!maybe_u32.is_null);
+    test_assert(0 == maybe_u32.value);
+    test_assert(!ns(ScalarStuff_just_u32_is_present(t)));
+    test_assert(ns(ScalarStuff_maybe_u32_is_present(t)));
+    test_assert(ns(ScalarStuff_default_u32_is_present(t)));
+
+    test_assert(42 == ns(ScalarStuff_just_f32_get(t)));
+    test_assert(42 == ns(ScalarStuff_maybe_f32_get(t)));
+    test_assert(42 == ns(ScalarStuff_default_f32_get(t)));
+    maybe_f32 = ns(ScalarStuff_maybe_f32_option(t));
+    test_assert(!maybe_f32.is_null);
+    test_assert(42 == maybe_f32.value);
+    test_assert(ns(ScalarStuff_just_f32_is_present(t)));
+    test_assert(ns(ScalarStuff_maybe_f32_is_present(t)));
+    test_assert(!ns(ScalarStuff_default_f32_is_present(t)));
+
+    test_assert(1 == ns(ScalarStuff_just_bool_get(t)));
+    test_assert(1 == ns(ScalarStuff_maybe_bool_get(t)));
+    test_assert(1 == ns(ScalarStuff_default_bool_get(t)));
+    maybe_bool = ns(ScalarStuff_maybe_bool_option(t));
+    test_assert(!maybe_bool.is_null);
+    test_assert(1 == maybe_bool.value);
+    test_assert(ns(ScalarStuff_just_bool_is_present(t)));
+    test_assert(ns(ScalarStuff_maybe_bool_is_present(t)));
+    test_assert(!ns(ScalarStuff_default_bool_is_present(t)));
+
+    test_assert(1 == ns(ScalarStuff_just_enum_get(t)));
     maybe_enum = ns(ScalarStuff_maybe_enum_option(t));
-    test_assert(maybe_enum.is_null);
+    test_assert(1 == ns(ScalarStuff_default_enum_get(t)));
+    test_assert(!maybe_enum.is_null);
+    test_assert(maybe_enum.value == 1);
+    test_assert(ns(ScalarStuff_just_enum_is_present(t)));
+    test_assert(ns(ScalarStuff_maybe_enum_is_present(t)));
+    test_assert(!ns(ScalarStuff_default_enum_is_present(t)));
 
     return 0;
 }
