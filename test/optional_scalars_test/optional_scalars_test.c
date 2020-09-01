ea595d2413a0e20f40d92a707bf06795bf02b55c
Mikkel Fahnøe Jørgensen <mikkel@dvide.com>
2020-09-01 20:47:57 +0200

    Draft optional scalar json parser test

--- test/optional_scalars_test/optional_scalars_test.c
+++ test/optional_scalars_test/optional_scalars_test.c
@@ -3,12 +3,13 @@
 
 #include "optional_scalars_test_builder.h"
 #include "optional_scalars_test_json_printer.h"
+#include "optional_scalars_test_json_parser.h"
 
 
 #undef ns
 #define ns(x) FLATBUFFERS_WRAP_NAMESPACE(optional_scalars, x)
 
-// #define TEST_ASSERT
+ #define TEST_ASSERT
 
 #ifdef TEST_ASSERT
 #define test_assert(x) do { if (!(x)) { assert(0); return -1; }} while(0)
@@ -16,42 +17,42 @@
 #define test_assert(x) do { if (!(x)) { return -1; }} while(0)
 #endif
 
-int create_scalar_stuff(flatcc_builder_t *B)
+int create_scalar_stuff(flatcc_builder_t *builder)
 {
-    ns(ScalarStuff_start_as_root(B));
+    ns(ScalarStuff_start_as_root(builder));
 
-    ns(ScalarStuff_just_i8_add(B, 10));
-    ns(ScalarStuff_maybe_i8_add(B, 11));
-    ns(ScalarStuff_default_i8_add(B, 12));
+    ns(ScalarStuff_just_i8_add(builder, 10));
+    ns(ScalarStuff_maybe_i8_add(builder, 11));
+    ns(ScalarStuff_default_i8_add(builder, 12));
 
-    ns(ScalarStuff_just_i16_add(B, 42));
-    ns(ScalarStuff_maybe_i16_add(B, 42));
-    ns(ScalarStuff_default_i16_add(B, 42));
+    ns(ScalarStuff_just_i16_add(builder, 42));
+    ns(ScalarStuff_maybe_i16_add(builder, 42));
+    ns(ScalarStuff_default_i16_add(builder, 42));
 
-    ns(ScalarStuff_just_u32_add(B, 0));
-    ns(ScalarStuff_maybe_u32_add(B, 0));
-    ns(ScalarStuff_default_u32_add(B, 0));
+    ns(ScalarStuff_just_u32_add(builder, 0));
+    ns(ScalarStuff_maybe_u32_add(builder, 0));
+    ns(ScalarStuff_default_u32_add(builder, 0));
 
-    ns(ScalarStuff_just_f32_add(B, 42));
-    ns(ScalarStuff_maybe_f32_add(B, 42));
-    ns(ScalarStuff_default_f32_add(B, 42));
+    ns(ScalarStuff_just_f32_add(builder, 42));
+    ns(ScalarStuff_maybe_f32_add(builder, 42));
+    ns(ScalarStuff_default_f32_add(builder, 42));
 
-    ns(ScalarStuff_just_bool_add(B, 1));
-    ns(ScalarStuff_maybe_bool_add(B, 1));
-    ns(ScalarStuff_default_bool_add(B, 1));
+    ns(ScalarStuff_just_bool_add(builder, 1));
+    ns(ScalarStuff_maybe_bool_add(builder, 1));
+    ns(ScalarStuff_default_bool_add(builder, 1));
 
-    ns(ScalarStuff_just_enum_add)(B, ns(OptionalByte_One));
-    ns(ScalarStuff_maybe_enum_add)(B, ns(OptionalByte_One));
-    ns(ScalarStuff_default_enum_add)(B, ns(OptionalByte_One));
+    ns(ScalarStuff_just_enum_add)(builder, ns(OptionalByte_One));
+    ns(ScalarStuff_maybe_enum_add)(builder, ns(OptionalByte_One));
+    ns(ScalarStuff_default_enum_add)(builder, ns(OptionalByte_One));
 
-    ns(ScalarStuff_end_as_root(B));
+    ns(ScalarStuff_end_as_root(builder));
 
     return 0;
 }
 
-int access_scalar_stuff(const void *buffer)
+int access_scalar_stuff(const void *buf)
 {
-    ns(ScalarStuff_table_t) t = ns(ScalarStuff_as_root(buffer));
+    ns(ScalarStuff_table_t) t = ns(ScalarStuff_as_root(buf));
     flatbuffers_int8_option_t maybe_i8;
     flatbuffers_int16_option_t maybe_i16;
     flatbuffers_uint32_option_t maybe_u32;
@@ -151,7 +152,7 @@ int test()
 const char *expected_json =
 "{\"just_i8\":10,\"maybe_i8\":11,\"default_i8\":12,\"just_i16\":42,\"maybe_i16\":42,\"maybe_u32\":0,\"default_u32\":0,\"just_f32\":42,\"maybe_f32\":42,\"just_bool\":true,\"maybe_bool\":true,\"just_enum\":\"One\",\"maybe_enum\":\"One\"}";
 
-int test_json()
+int test_json_printer()
 {
     flatcc_builder_t builder;
     void  *buf;
@@ -172,7 +173,44 @@ int test_json()
     printf("%.*s\n", (int)json_size, json_buf);
     test_assert(strlen(expected_json) == json_size);
     test_assert(0 == memcmp(expected_json, json_buf, json_size));
+
+
     flatcc_json_printer_clear(&printer);
+    return 0;
+}
+
+int print_buffer(const void *buf, size_t size)
+{
+    flatcc_json_printer_t printer;
+    flatcc_json_printer_init(&printer, 0);
+    if (ns(ScalarStuff_print_json_as_root)(&printer, buf, size, NULL)) {
+        printf("error printing buffer");
+        return -1;
+    }
+    return 0;
+}
+
+int test_json_parser()
+{
+    flatcc_builder_t builder;
+    void  *buf;
+    size_t size;
+    flatcc_json_parser_t parser;
+    int ret;
+
+    flatcc_builder_init(&builder);
+    ret = optional_scalars_ScalarStuff_parse_json_as_root(&builder,
+            &parser, expected_json, strlen(expected_json), 0, 0);
+    test_assert(ret == 0);
+
+    buf = flatcc_builder_finalize_aligned_buffer(&builder, &size);
+
+    // TODO: parsed content is all wrong
+    print_buffer(buf, size);
+    test_assert(0 == access_scalar_stuff(buf));
+
+    flatcc_builder_aligned_free(buf);
+    flatcc_builder_clear(&builder);
     return 0;
 }
 
@@ -186,8 +224,12 @@ int main(int argc, char *argv[])
         printf("optional scalars test failed");
         return 1;
     }
-    if (test_json()) {
-        printf("optional scalars json test failed");
+    if (test_json_printer()) {
+        printf("optional scalars json printer test failed");
+        return 1;
+    }
+    if (test_json_parser()) {
+        printf("optional scalars json parser test failed");
         return 1;
     }
     printf("optional scalars test passed");
