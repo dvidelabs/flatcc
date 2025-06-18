b59f8a1e792278a8eee99113bba56a22bd9cd83f
Jeff Katz <github@kraln.com>
2025-06-18 19:54:21 +0200

    Fixup warning on GCC_VERSION: 15.1.1

--- include/flatcc/portable/grisu3_print.h
+++ include/flatcc/portable/grisu3_print.h
@@ -183,7 +183,7 @@ static int grisu3_i_to_str(int val, char *str)
 
 static int grisu3_print_nan(uint64_t v, char *dst)
 {
-    static char hexdigits[16] = "0123456789ABCDEF";
+    static char hexdigits[16] __attribute__((nonstring)) = "0123456789ABCDEF";
     int i = 0;
 
     dst[0] = 'N';
