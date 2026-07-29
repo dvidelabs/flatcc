09fc109ad871da6458e25baa4b328581f7cdbcc6
Mikkel Fahnøe Jørgensen <mikkel@dvide.com>
2026-07-29 11:33:54 +0200

    Start development 0.6.4-pre

--- include/flatcc/flatcc_version.h
+++ include/flatcc/flatcc_version.h
@@ -2,12 +2,12 @@
 extern "C" {
 #endif
 
-#define FLATCC_VERSION_TEXT "0.6.3"
+#define FLATCC_VERSION_TEXT "0.6.4-pre"
 #define FLATCC_VERSION_MAJOR 0
 #define FLATCC_VERSION_MINOR 6
-#define FLATCC_VERSION_PATCH 3
+#define FLATCC_VERSION_PATCH 4
 /* 1 or 0 */
-#define FLATCC_VERSION_RELEASED 1
+#define FLATCC_VERSION_RELEASED 0
 
 #ifdef __cplusplus
 }
